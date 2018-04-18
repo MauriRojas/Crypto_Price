98cf92d849371f1f9628d05d600dbe210e898a9d
MauriRojas <31770211+MauriRojas@users.noreply.github.com>
2018-04-18 18:52:07 -0300

    Update
    Now when clicked, the price updates the value of the currency.

--- popup/precio_actual.js
+++ popup/precio_actual.js
@@ -1,14 +1,19 @@
-var json = new XMLHttpRequest();
-json.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var object = JSON.parse(this.responseText);
-    var found = object.filter(function(item) { return item.id === "bitcoin"; });
-    document.getElementById("valor").innerHTML = "$" + found[0].price_usd;
-  }
-};
-json.open(
-  "GET",
-  "https://api.coinmarketcap.com/v1/ticker/?convert=USD&limit=10",
-  true
-);
-json.send();
+function getValue() {
+  document.getElementById("valor").innerHTML = "Loading";
+  var json = new XMLHttpRequest();
+  json.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var object = JSON.parse(this.responseText);
+      var found = object.filter(function(item) { return item.id === "bitcoin"; });
+      document.getElementById("valor").innerHTML = "$" + found[0].price_usd;
+    }
+  };
+  json.open(
+    "GET",
+    "https://api.coinmarketcap.com/v1/ticker/?convert=USD&limit=10",
+    true
+  );
+  json.send();
+}
+getValue();
+document.getElementById("valor").addEventListener("click", function() {getValue()});
